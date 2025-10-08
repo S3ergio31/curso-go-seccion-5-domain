39653860e3ea1e9764ee7136c859d09fbb795c73
Sergio Fidelis <sergiofidelis31@hotmail.com>
2025-10-08 00:34:31 -0300

    add enroll status

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Enrollment struct {
-	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key:unique_index"`
-	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36);not null"`
-	CourseID  string     `json:"course_id" gorm:"type:char(36);not null"`
-	Status    string     `json:"status" gorm:"type:char(2)"`
-	CreatedAt *time.Time `json:"-"`
-	UpdatedAt *time.Time `json:"-"`
+	ID        string           `json:"id" gorm:"type:char(36);not null;primary_key:unique_index"`
+	UserID    string           `json:"user_id,omitempty" gorm:"type:char(36);not null"`
+	CourseID  string           `json:"course_id" gorm:"type:char(36);not null"`
+	Status    EnrollmentStatus `json:"status" gorm:"type:char(2)"`
+	CreatedAt *time.Time       `json:"-"`
+	UpdatedAt *time.Time       `json:"-"`
 }
 
 func (u *Enrollment) BeforeCreate(tx *gorm.DB) error {
@@ -22,3 +22,12 @@ func (u *Enrollment) BeforeCreate(tx *gorm.DB) error {
 	}
 	return nil
 }
+
+type EnrollmentStatus string
+
+const (
+	Pending  EnrollmentStatus = "P"
+	Active   EnrollmentStatus = "A"
+	Studying EnrollmentStatus = "S"
+	Inactive EnrollmentStatus = "I"
+)
